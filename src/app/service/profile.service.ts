3ee7c24ae0d9a625b9775e5c119fc0d0ef6917c8
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-10-17 11:29:48 +0300

    This commit adds the getUsers method in the profile service

--- src/app/service/profile.service.ts
+++ src/app/service/profile.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+user: User;
+repos: any;
+username = 'Ckhajira';
+searchname ="tetris"
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.user =new User('',0,'');
+    this.http = http;
+  }
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,5 @@
 export class User {
+    constructor(public login:string, public id:number, public avatar_url:string){
+        
+    }
 }
